abf7e102609a5326223385e7344cd873a5fbb1bb
Ashwinkumar R <ashwin@artifintel.in>
2021-12-09 20:47:57 +0530

    Added codes

---
+++ app.py
@@ -0,0 +1,11 @@
+def main():
+    adhaar=[]
+    print("Welcome to Railways Reservation System")
+    name=input("Enter your name:")
+    age=int(input("Enter the age:"))
+    adhaar_card=int(input("Enter the Adhaar Card Number:"))
+    adhaar.append(adhaar_card)
+    
+
+
+        
